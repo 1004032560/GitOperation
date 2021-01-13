ebfaa62b4ffa93c4febb03f3c62d879de9907d4d
wanghaojun <1004032560@qq.com>
2021-01-13 10:46:09 +0800

    JavaGitTest

---
+++ JavaProject/JavaGit/JavaGitTest/src/main/java/com/looper/controller/DemoController.java
@@ -0,0 +1,11 @@
+package com.looper.controller;
+
+public class DemoController {
+
+    public static void main(String[] args) {
+
+        System.out.println("DemoController");
+
+    }
+
+}
---
+++ JavaProject/JavaGit/JavaGitTest/src/main/java/com/looper/server/DemoServer.java
@@ -0,0 +1,11 @@
+package com.looper.server;
+
+public class DemoServer {
+
+    public static void main(String[] args) {
+
+        System.out.println("DemoServer");
+
+    }
+
+}
