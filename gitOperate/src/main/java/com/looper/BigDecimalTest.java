7d2f85d9e1f3e42baf95a5a3068fa969d4f3c1e9
wanghaojun <1004032560@qq.com>
2021-01-17 16:31:25 +0800

    update

---
+++ gitOperate/src/main/java/com/looper/BigDecimalTest.java
@@ -0,0 +1,12 @@
+package com.looper;
+
+import java.math.BigDecimal;
+
+public class BigDecimalTest {
+
+    public static void main(String[] args) {
+        BigDecimal bigDecimal = new BigDecimal(1);
+        System.out.println(bigDecimal.negate().intValue());
+    }
+
+}
